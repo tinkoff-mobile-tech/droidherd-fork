b5ecb0479c3c88c96dcbc76265ac115981ff5d59
Roman Torsten <r.torsten@tinkoff.ru>
2023-04-05 10:13:24 +0300

    Update README for example app and version of fork to release.
    Update docker runner script to use released docker image.

--- example-app/README.md
+++ example-app/README.md
@@ -1,1 +1,8 @@
-# TODO
+# Example app
+
+This is demo application created in Android Studio with applied droidherd-fork plugin.
+
+Use script [droidherd-run.sh](droidherd-run.sh) as helper to run tests and provide required parameters.
+
+- DROIDHERD_SERVICE_URL - URL of service, localhost:8080 by default
+- DROIDHERD_EMULATORS - emulators to request, android-29 and android-30 by default.
--- example-app/droidherd-run.sh
+++ example-app/droidherd-run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-export DROIDHERD_SERVICE_URL="http://localhost:31252"
+export DROIDHERD_SERVICE_URL=${DROIDHERD_SERVICE_URL:-"http://localhost:8080"}
+export DROIDHERD_EMULATORS=${DROIDHERD_EMULATORS:-"android-29:1,android-30:1"}
 
 ./gradlew \
   --no-daemon fork \
   -Pfork.droidherd.url="${DROIDHERD_SERVICE_URL}" \
-  -Pfork.droidherd.emulators="android-29:1,android-30:1"
+  -Pfork.droidherd.emulators=${DROIDHERD_EMULATORS}
