9c5931f6208e2c4dc03a64964dca29fa4ebf5c16
Roman Torsten <r.torsten@tinkoff.ru>
2023-04-05 10:13:24 +0300

    Update README for example app and version of fork to release.
    Update docker runner script to use released docker image.

--- run-fork-in-docker.sh
+++ run-fork-in-docker.sh
@@ -6,7 +6,7 @@ APK=${APK:-"/path-to/app-debug.apk"}
 TEST_APK=${TEST_APK:-"/path-to/app-debug-androidTest.apk"}
 DROIDHERD_EMULATORS=${DROIDHERD_EMULATORS:-"android-30:1"}
 DROIDHERD_URL=${DROIDHERD_URL:-"http://localhost:8080"}
-FORK_DOCKER_IMAGE=${FORK_DOCKER_IMAGE:-"droidherd-fork:local"}
+FORK_DOCKER_IMAGE=${FORK_DOCKER_IMAGE:-"tinkoffcreditsystems/droidherd-fork:1.0.0"}
 
 mkdir -p apks
 cp $APK apks/app.apk
